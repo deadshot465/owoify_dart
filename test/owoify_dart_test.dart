a0f01764d076eef67e814d13b45b7153a04a2722
Chehui Chou <deadshot465@gmail.com>
2021-05-03 20:57:52 +0900

    Add test.

--- test/owoify_dart_test.dart
+++ test/owoify_dart_test.dart
@@ -1,16 +1,42 @@
 import 'package:owoify_dart/owoify_dart.dart';
 import 'package:test/test.dart';
 
+final source = 'Hello World! This is the string to owo! Kinda cute, isn\'t it?';
+
 void main() {
-  group('A group of tests', () {
+  group('A group of owoify tests', () {
     Owoifier owoifier;
 
     setUp(() {
       owoifier = Owoifier();
     });
 
-    test('First Test', () {
-      expect(Owoifier.owoify('Hello World!'), isNotEmpty);
+    test('Test owoify', () {
+      expect(Owoifier.owoify(source), isNot(source));
+    });
+
+    test('Test owo', () {
+      expect(Owoifier.owoify(source), isNotEmpty);
+    });
+
+    test('Test uwu', () {
+      expect(Owoifier.owoify(source, level: OwoifyLevel.Uwu), isNotEmpty);
+    });
+
+    test('Test uvu', () {
+      expect(Owoifier.owoify(source, level: OwoifyLevel.Uvu), isNotEmpty);
+    });
+
+    test('Test owo not equal to uwu', () {
+      expect(Owoifier.owoify(source), isNot(Owoifier.owoify(source, level: OwoifyLevel.Uwu)));
+    });
+
+    test('Test owo not equal to uvu', () {
+      expect(Owoifier.owoify(source), isNot(Owoifier.owoify(source, level: OwoifyLevel.Uvu)));
+    });
+
+    test('Test uwu not equal to uvu', () {
+      expect(Owoifier.owoify(source, level: OwoifyLevel.Uwu), isNot(Owoifier.owoify(source, level: OwoifyLevel.Uvu)));
     });
   });
 }
