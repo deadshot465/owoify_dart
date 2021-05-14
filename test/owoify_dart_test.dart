3d19353eff3a3ac3806ceff2b2174d0a6d698e91
Chehui Chou <deadshot465@gmail.com>
2021-05-15 06:26:36 +0900

    Add more test cases.

--- test/owoify_dart_test.dart
+++ test/owoify_dart_test.dart
@@ -1,14 +1,17 @@
+import 'dart:io';
+
 import 'package:owoify_dart/owoify_dart.dart';
 import 'package:test/test.dart';
 
-final source = 'Hello World! This is the string to owo! Kinda cute, isn\'t it?';
-
 void main() {
   group('A group of owoify tests', () {
-    Owoifier owoifier;
+    final source = 'Hello World! This is the string to owo! Kinda cute, isn\'t it?';
+    final pokemonNameListPath = 'assets/pokemons.txt';
+    final warAndPeacePath = 'assets/war_and_peace_chapter01-20.txt';
+    final pokemonNameList = File(pokemonNameListPath).readAsLinesSync();
+    final warAndPeaceText = File(warAndPeacePath).readAsStringSync();
 
     setUp(() {
-      owoifier = Owoifier();
     });
 
     test('Test owoify', () {
@@ -16,15 +19,21 @@ void main() {
     });
 
     test('Test owo', () {
-      expect(Owoifier.owoify(source), isNotEmpty);
+      final text = Owoifier.owoify(source);
+      expect(text, isNotEmpty);
+      expect(text, isNotNull);
     });
 
     test('Test uwu', () {
-      expect(Owoifier.owoify(source, level: OwoifyLevel.Uwu), isNotEmpty);
+      final text = Owoifier.owoify(source, level: OwoifyLevel.Uwu);
+      expect(text, isNotEmpty);
+      expect(text, isNotNull);
     });
 
     test('Test uvu', () {
-      expect(Owoifier.owoify(source, level: OwoifyLevel.Uvu), isNotEmpty);
+      final text = Owoifier.owoify(source, level: OwoifyLevel.Uvu);
+      expect(text, isNotEmpty);
+      expect(text, isNotNull);
     });
 
     test('Test owo not equal to uwu', () {
@@ -37,6 +46,32 @@ void main() {
 
     test('Test uwu not equal to uvu', () {
       expect(Owoifier.owoify(source, level: OwoifyLevel.Uwu), isNot(Owoifier.owoify(source, level: OwoifyLevel.Uvu)));
+    });
+
+    test('Test pokemon names', () {
+      pokemonNameList.forEach((element) {
+        final textWithOwo = Owoifier.owoify(element);
+        final textWithUwu = Owoifier.owoify(element, level: OwoifyLevel.Uwu);
+        final textWithUvu = Owoifier.owoify(element, level: OwoifyLevel.Uvu);
+        expect(textWithOwo, isNotEmpty);
+        expect(textWithUwu, isNotEmpty);
+        expect(textWithUvu, isNotEmpty);
+        expect(textWithOwo, isNotNull);
+        expect(textWithUwu, isNotNull);
+        expect(textWithUvu, isNotNull);
+      });
+    });
+
+    test('Test long text', () {
+      final textWithOwo = Owoifier.owoify(warAndPeaceText);
+      final textWithUwu = Owoifier.owoify(warAndPeaceText, level: OwoifyLevel.Uwu);
+      final textWithUvu = Owoifier.owoify(warAndPeaceText, level: OwoifyLevel.Uvu);
+      expect(textWithOwo, isNotEmpty);
+      expect(textWithUwu, isNotEmpty);
+      expect(textWithUvu, isNotEmpty);
+      expect(textWithOwo, isNotNull);
+      expect(textWithUwu, isNotNull);
+      expect(textWithUvu, isNotNull);
     });
   });
 }
